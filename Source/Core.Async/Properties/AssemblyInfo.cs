d01bd43e7d11d7e617075d5a04d939a7d88db004
DanHarman <daniel.a.harman@gmail.com>
2014-03-12 14:31:48 +0000

    Recreated Core.Async project to resolve resharper fail.

--- Source/Core.Async/Properties/AssemblyInfo.cs
+++ Source/Core.Async/Properties/AssemblyInfo.cs
@@ -1,29 +1,16 @@
-﻿// <copyright file="AssemblyInfo.cs" company="million miles per hour ltd">
-// Copyright (c) 2013-2014 All Right Reserved
-// 
-// This source is subject to the MIT License.
-// Please see the License.txt file for more information.
-// All other rights reserved.
-// 
-// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
-// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-// PARTICULAR PURPOSE.
-// 
-// </copyright>
-
+﻿using System.Resources;
 using System.Reflection;
-using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Core.Rx")]
+[assembly: AssemblyTitle("Core.Async")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Core.Rx")]
+[assembly: AssemblyProduct("Core.Async")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -39,6 +26,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
