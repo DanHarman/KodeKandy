265dd8271489a695e0e2264806b45e72f73531ca
Daniel Harman <daniel.harman@trafigura.com>
2014-03-24 10:45:20 +0000

    Added comment.

--- Source/Mapnificent/Helpers.cs
+++ Source/Mapnificent/Helpers.cs
@@ -2,6 +2,11 @@ namespace KodeKandy.Mapnificent
 {
     public static class Helpers
     {
+        /// <summary>
+        /// Safely get the name of a type returning <![CDATA[<null>]]> if the instance is null.
+        /// </summary>
+        /// <param name="instance">Instance to return the type of.</param>
+        /// <returns>The name of the type or <![CDATA[<null>]]>.</returns>
         public static string SafeGetTypeName(this object instance)
         {
             return instance == null ? "<null>" : instance.GetType().Name;
