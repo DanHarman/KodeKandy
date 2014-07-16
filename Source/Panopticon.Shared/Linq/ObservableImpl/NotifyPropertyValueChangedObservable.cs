989c25acd33efd44eaa6a52897e757960aba3b9a
Dan Harman <daniel.a.harman+github@gmail.com>
2014-07-16 11:24:00 +0100

    Refactor for more reuse

--- Source/Panopticon.Shared/Linq/ObservableImpl/NotifyPropertyValueChangedObservable.cs
+++ Source/Panopticon.Shared/Linq/ObservableImpl/NotifyPropertyValueChangedObservable.cs
@@ -18,6 +18,11 @@ using KodeKandy.Panopticon.Internal;
 
 namespace KodeKandy.Panopticon.Linq.ObservableImpl
 {
+    interface IAdaptor<in TFrom, out TTo> : IObserver<TFrom>, IDisposable
+    {
+        void NotifyInitialValue(IObserver<TTo> observer);
+    }
+
     /// <summary>
     ///     An observable that watches a class and provides an observable for a property on that class, if the class implement
     ///     <see cref="INotifyPropertyChanged" />. If the class does not implement that interface, then only the initial value
@@ -33,13 +38,13 @@ namespace KodeKandy.Panopticon.Linq.ObservableImpl
     /// <typeparam name="TClass">The type of the observered clas.</typeparam>
     /// <typeparam name="TProperty">The type of the observered property.</typeparam>
     internal class NotifyPropertyValueChangedObservable<TClass, TProperty> :
-        NotifyPropertyChangedObservableBase<TClass, IPropertyValueChanged<TProperty>>, IObserver<IPropertyValueChanged<TClass>>,
+        NotifyPropertyChangedObservableBase<TClass, IPropertyValueChanged<TProperty>>,
         IObservable<IPropertyValueChanged<TProperty>>
         where TClass : class
     {
         private readonly string _propertyName;
         private readonly Func<TClass, TProperty> _propertyValueGetter;
-        private IPropertyValueChanged<TProperty> _currentPropertyValueChanged;
+        private IAdaptor<IPropertyValueChanged<TClass>, IPropertyValueChanged<TProperty>> _adaptor;
 
         public NotifyPropertyValueChangedObservable(IObservable<IPropertyValueChanged<TClass>> sourceObservable, string propertyName,
             Func<TClass, TProperty> propertyValueGetter) : base(sourceObservable)
@@ -72,7 +77,8 @@ namespace KodeKandy.Panopticon.Linq.ObservableImpl
                     {
                         // If we have no _observer then make it our _observer and subscribe to _sourceObservable.
                         Observer = observer;
-                        SourceSubscriptionDisposable = SourceObservable.Subscribe(this);
+                        _adaptor = new NotifyPropertyValueChangedAdaptor(this);
+                        SourceSubscriptionDisposable = SourceObservable.Subscribe(_adaptor);
                     }
                     else
                     {
@@ -94,7 +100,7 @@ namespace KodeKandy.Panopticon.Linq.ObservableImpl
                         // Send the new observer the current property value. This is done inside the lock to prevent race conditions around the initial
                         // value. Behaviour subject does similar, although I'm not sure we need to make this guarantee as it is stronger than the
                         // guarantee for the property changing once already subscribed (whereby ordering is not guaranteed).
-                        observer.OnNext(_currentPropertyValueChanged);
+                        _adaptor.NotifyInitialValue(observer);
                     }
 
                     return new Subscription(this, observer);
@@ -113,104 +119,130 @@ namespace KodeKandy.Panopticon.Linq.ObservableImpl
 
         #region IObserver<IPropertyValueChanged<TClass>> Members
 
-        void IObserver<IPropertyValueChanged<TClass>>.OnNext(IPropertyValueChanged<TClass> newSourcePropertyValueChanged)
+        class NotifyPropertyValueChangedAdaptor : IAdaptor<IPropertyValueChanged<TClass>, IPropertyValueChanged<TProperty>>
         {
-            IObserver<IPropertyValueChanged<TProperty>> oldObserver;
-            TClass oldSource;
-            IPropertyValueChanged<TProperty> initialPropertyValueChanged;
+            private readonly NotifyPropertyValueChangedObservable<TClass, TProperty> _observable;
+            private TClass _source;
+            private IPropertyValueChanged<TProperty> _currentPropertyValueChanged;
 
-            lock (Gate)
+            public NotifyPropertyValueChangedAdaptor(NotifyPropertyValueChangedObservable<TClass, TProperty> observable)
             {
-                if (Observer is CompletedObserver<IPropertyValueChanged<TProperty>>)
-                    return;
-
-                oldObserver = Observer;
-                oldSource = Source;
-                Source = newSourcePropertyValueChanged.Value;
-
-                // We need to get the current property value from this new source if it is not null, but if it is propagate the nullness
-                // with a PropertyValueChanged with HasValue == false.
-                initialPropertyValueChanged = Source != null
-                    ? PropertyValueChanged.CreateWithValue(Source, _propertyName, _propertyValueGetter(Source))
-                    : PropertyValueChanged.CreateWithoutValue<TClass, TProperty>(Source, _propertyName);
-
-                _currentPropertyValueChanged = initialPropertyValueChanged;
+                _observable = observable;
             }
 
-            // Connect to INotifyPropertyChanged on the new _source.
-            var newSourceAsNotifyPropertyChanged = newSourcePropertyValueChanged.Value as INotifyPropertyChanged;
-            if (newSourceAsNotifyPropertyChanged != null)
-                newSourceAsNotifyPropertyChanged.PropertyChanged += OnPropertyChanged;
+            public void OnNext(IPropertyValueChanged<TClass> newSourcePropertyValueChanged)
+            {
+                IObserver<IPropertyValueChanged<TProperty>> oldObserver;
+                TClass oldSource;
+                IPropertyValueChanged<TProperty> initialPropertyValueChanged;
 
-            // Disconnect from INotifyPropertyChanged on previous _source. Because _source has been changed it doesn't matter
-            // if we end up with an extra event from this old sourceObservable sneaking through as it will be filtered out.
-            var oldSourceAsNotifyPropertyChanged = oldSource as INotifyPropertyChanged;
-            if (oldSourceAsNotifyPropertyChanged != null)
-                oldSourceAsNotifyPropertyChanged.PropertyChanged -= OnPropertyChanged;
+                lock (_observable.Gate)
+                {
+                    if (_observable.Observer is CompletedObserver<IPropertyValueChanged<TProperty>>)
+                        return;
 
-            oldObserver.OnNext(initialPropertyValueChanged);
-        }
+                    oldObserver = _observable.Observer;
+                    oldSource = _source;
+                    _source = newSourcePropertyValueChanged.Value;
 
-        void IObserver<IPropertyValueChanged<TClass>>.OnError(Exception error)
-        {
-            base.OnError(error);
-        }
+                    // We need to get the current property value from this new source if it is not null, but if it is propagate the nullness
+                    // with a PropertyValueChanged with HasValue == false.
+                    initialPropertyValueChanged = _source != null
+                        ? PropertyValueChanged.CreateWithValue(_source, _observable._propertyName, _observable._propertyValueGetter(_source))
+                        : PropertyValueChanged.CreateWithoutValue<TClass, TProperty>(_source, _observable._propertyName);
 
-        void IObserver<IPropertyValueChanged<TClass>>.OnCompleted()
-        {
-            base.OnCompleted();
+                    _currentPropertyValueChanged = initialPropertyValueChanged;
+                }
+
+                // Connect to INotifyPropertyChanged on the new _source.
+                var newSourceAsNotifyPropertyChanged = newSourcePropertyValueChanged.Value as INotifyPropertyChanged;
+                if (newSourceAsNotifyPropertyChanged != null)
+                    newSourceAsNotifyPropertyChanged.PropertyChanged += OnPropertyChanged;
+
+                // Disconnect from INotifyPropertyChanged on previous _source. Because _source has been changed it doesn't matter
+                // if we end up with an extra event from this old sourceObservable sneaking through as it will be filtered out.
+                var oldSourceAsNotifyPropertyChanged = oldSource as INotifyPropertyChanged;
+                if (oldSourceAsNotifyPropertyChanged != null)
+                    oldSourceAsNotifyPropertyChanged.PropertyChanged -= OnPropertyChanged;
+
+                oldObserver.OnNext(initialPropertyValueChanged);
+            }
+
+            public void OnError(Exception error)
+            {
+                _observable.OnError(error);
+            }
+
+            public void OnCompleted()
+            {
+                _observable.OnCompleted();
+            }
+
+            public void NotifyInitialValue(IObserver<IPropertyValueChanged<TProperty>> observer)
+            {
+                observer.OnNext(_currentPropertyValueChanged); 
+            }
+
+            /// <summary>
+            ///     Internal <see cref="PropertyChangedEventHandler" /> to bind to the targets PropertyChanged event.
+            ///     Gets the current property value from the current sourceObservable and pushes it to the observers.
+            /// </summary>
+            /// <remarks>
+            ///     We keep a local copy of the current property value so that we don't end up with concurrency issues which
+            ///     would occur if we pulled it form the sourceObservable on demand e.g. when adding a new observer, as
+            ///     we do not have a lock around the sourceObservable, but we can control when it pushes new values into us with our
+            ///     lock.
+            /// </remarks>
+            private void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+            {
+                IObserver<IPropertyValueChanged<TProperty>> currObserver;
+                IPropertyValueChanged<TProperty> propertyValueChanged;
+
+                lock (_observable.Gate)
+                {
+                    if (_source != sender || _observable._propertyName != propertyChangedEventArgs.PropertyName)
+                        return;
+
+                    currObserver = _observable.Observer;
+                    propertyValueChanged = PropertyValueChanged.CreateWithValue(_source, propertyChangedEventArgs, _observable._propertyValueGetter(_source));
+                    _currentPropertyValueChanged = propertyValueChanged;
+                }
+
+                currObserver.OnNext(propertyValueChanged);
+            }
+
+            public void Dispose()
+            {
+                var sourceAsNotifyPropertyChanged = _source as INotifyPropertyChanged;
+                if (sourceAsNotifyPropertyChanged != null)
+                {
+                    sourceAsNotifyPropertyChanged.PropertyChanged -= OnPropertyChanged;
+                }
+
+                _source = null;
+                _currentPropertyValueChanged = null;
+            }
         }
 
         #endregion
-
-        /// <summary>
-        ///     Internal <see cref="PropertyChangedEventHandler" /> to bind to the targets PropertyChanged event.
-        ///     Gets the current property value from the current sourceObservable and pushes it to the observers.
-        /// </summary>
-        /// <remarks>
-        ///     We keep a local copy of the current property value so that we don't end up with concurrency issues which
-        ///     would occur if we pulled it form the sourceObservable on demand e.g. when adding a new observer, as
-        ///     we do not have a lock around the sourceObservable, but we can control when it pushes new values into us with our
-        ///     lock.
-        /// </remarks>
-        private void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
-        {
-            IObserver<IPropertyValueChanged<TProperty>> currObserver;
-            IPropertyValueChanged<TProperty> propertyValueChanged;
-
-            lock (Gate)
-            {
-                if (Source != sender || _propertyName != propertyChangedEventArgs.PropertyName)
-                    return;
-
-                currObserver = Observer;
-                propertyValueChanged = PropertyValueChanged.CreateWithValue(Source, propertyChangedEventArgs, _propertyValueGetter(Source));
-                _currentPropertyValueChanged = propertyValueChanged;
-            }
-
-            currObserver.OnNext(propertyValueChanged);
-        }
-
+        
         /// <summary>
         ///     Cleans up by unsubscribing to the sourceObservable, release references & etc.
         /// </summary>
         /// <remarks>Must be called inside the lock.</remarks>
         protected override void CleanUp()
         {
-            var sourceAsNotifyPropertyChanged = Source as INotifyPropertyChanged;
-            if (sourceAsNotifyPropertyChanged != null)
-            {
-                sourceAsNotifyPropertyChanged.PropertyChanged -= OnPropertyChanged;
-            }
-
             if (SourceSubscriptionDisposable != null)
             {
                 SourceSubscriptionDisposable.Dispose();
                 SourceSubscriptionDisposable = null;
             }
 
-            Source = null;
-            _currentPropertyValueChanged = null;
+            if (_adaptor != null)
+            {
+                _adaptor.Dispose();
+                _adaptor = null;
+            }
         }
     }
 }
